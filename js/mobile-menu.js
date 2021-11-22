57b78589b8d1d39c72a8fbc1fded340abcbe5cbf
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-11-22 14:40:55 +0200

    add mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+    const refs = {
+        openMenuBtn: document.querySelector(".menu-open"),
+        closeMenuBtn: document.querySelector(".menu-close"),
+        menu: document.querySelector(".menu-container"),
+    };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+    function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden");
+    }
 })();
